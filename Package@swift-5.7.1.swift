8c8e07d4ee1e9b590646601e9ffd64c5325bd973
Paul Tavitian <paul.tavitian@icloud.com>
2024-10-15 15:10:33 +1100

    Create Package@swift-5.7.1.swift

---
+++ Package@swift-5.7.1.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 5.7.1
+
+import PackageDescription
+
+let package = Package(
+	name: "KindleDeDRMTools",
+	platforms: [
+		.iOS(.v12),
+		.macOS(.v10_13)
+	],
+	products: [
+		.library(
+			name: "KindleDeDRMTools",
+			targets: ["KindleDeDRMTools"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.1.4")),
+		.package(url: "https://github.com/apple/swift-algorithms.git", .upToNextMajor(from: "1.2.0")),
+		.package(url: "https://github.com/themuzzleflare/kfxtablesswift.git", .upToNextMajor(from: "1.0.0")),
+		.package(url: "https://github.com/weichsel/ZIPFoundation.git", .upToNextMajor(from: "0.9.19")),
+		.package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.8.3")),
+		.package(url: "https://github.com/themuzzleflare/OSInfo.git", .upToNextMajor(from: "1.0.0"))
+	],
+	targets: [
+		.target(
+			name: "KindleDeDRMTools",
+			dependencies: [
+				.product(name: "OrderedCollections", package: "swift-collections"),
+				.product(name: "Algorithms", package: "swift-algorithms"),
+				.product(name: "KFXTables", package: "kfxtablesswift"),
+				"ZIPFoundation",
+				"CryptoSwift",
+				"OSInfo"
+			]
+		)
+	]
+)
