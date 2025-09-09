ed970dea49b140cd4cf27ebd2ccfc2f4ec987278
Paul Tavitian <paul.tavitian@icloud.com>
2025-09-09 14:44:52 +1000

    Update Package@swift-5.7.1.swift

--- Package@swift-5.7.1.swift
+++ Package@swift-5.7.1.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["KindleDeDRMTools"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.2.1")),
+        .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.1.6")),
         .package(url: "https://github.com/themuzzleflare/kfxtablesswift.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", .upToNextMajor(from: "0.9.19")),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.9.0")),
